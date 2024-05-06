bc5b1bf18939df60b37796c47772c16dbd305ab2
hyeneung <dkwjd0824@khu.ac.kr>
2024-05-06 17:03:58 +0900

    feat: logging

--- utils/xmlHandler.go
+++ utils/xmlHandler.go
@@ -45,22 +45,3 @@ func getLastIdxToUpdate(posts *ParsedData, updatedDate int64) int8 {
 	}
 	return index - 1
 }
-
-func insertDB(item Item) error {
-	// fmt.Println("업데이트할 아이템", item)
-	return nil
-}
-
-func UpdateDB(url string, updatedDate int64) (int8, int8) {
-	posts := getParsedData(url)
-	var updatedCount int8 = 0
-	lastIndex := getLastIdxToUpdate(posts, updatedDate)
-	for idx := int8(0); idx <= lastIndex; idx++ {
-		var item Item = posts.Items[idx]
-		err := insertDB(item)
-		if err == nil {
-			updatedCount += 1
-		}
-	}
-	return lastIndex + 1, updatedCount
-}
